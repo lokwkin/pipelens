8059c0a6db2072362df87d129f17007d3d3efc75
lokwkin <lokwkin@gmail.com>
2024-08-22 19:02:03 +0800

    feat: initial commit

---
+++ sample/test.ts
@@ -0,0 +1,51 @@
+import { StepTracker } from '../dist';
+
+const fetch = (url: string) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(url);
+        }, Math.floor(Math.random() * 3000) + 500);
+    });
+}
+
+async function main() {
+        
+    const mainTracker = new StepTracker('main');
+
+    await mainTracker.track(async (st: StepTracker) => {
+       
+        await st.step('demo', async (st: StepTracker) => {
+        
+            // Your logic here
+            st.log('foo', 'bar');
+            await new Promise(resolve => setTimeout(resolve, 200));
+        });
+        
+        const urls = ['https://url1.com', 'https://url2.com', 'https://url3.com'];
+        await st.step('fetch', async (st: StepTracker) => {
+                
+            await st.step('preprocess', async (st: StepTracker) => {
+                
+                // Some preprocess logic
+                st.log('someData', 12345);
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            });
+        
+            await new Promise(resolve => setTimeout(resolve, 500));
+        
+            // Concurrent sub steps
+            await Promise.all(urls.map(async (url) => {
+                return st.step(`${url}`, async (st: StepTracker) => {
+                    return await fetch(url);
+                });
+            }));
+        });
+    });
+    
+    console.log(JSON.stringify(mainTracker.output(), null, 2));
+
+    console.log(mainTracker.ganttUrl());
+
+}
+
+main();
---
+++ src/index.ts
@@ -0,0 +1,116 @@
+export type TimeMeta = {
+    startTs: number;
+    endTs: number;
+    timeUsageMs?: number;
+};
+
+export type StepMeta = {
+    key: string;
+    time: TimeMeta;
+    record: Record<string, any>;
+    result: any;
+};
+
+export class StepTracker {
+    
+    public key: string;
+    public records: Record<string, any>;
+    public result?: any;
+    public time: TimeMeta;
+    private subtrackers: { [key: string]: StepTracker } = {};
+    private ctx: StepTracker;
+    
+    constructor(key: string) {
+        this.key = key;
+        this.records = {};
+        this.time = {
+            startTs: Date.now(),
+            endTs: Date.now(),
+            timeUsageMs: 0
+        };
+        this.ctx = this;
+    }
+
+    private async run(callable: (st: StepTracker) => Promise<any>) {
+        this.time.startTs = Date.now();
+        this.result = await callable(this.ctx);
+        this.time.endTs = Date.now();
+        this.time.timeUsageMs = this.time.endTs - this.time.startTs;
+        return this.result;
+    }
+
+    public async track(callable: (st: StepTracker) => Promise<any>) {
+        return await this.run(callable);
+    }
+
+    public async step(key: string, callable: (st: StepTracker) => Promise<any>) {
+        const subtracker = new StepTracker(`${this.key}.${key}`);
+        this.subtrackers[key] = subtracker;
+        return await subtracker.run(callable);
+    }
+
+    public log(key: string, data: any) {
+        this.records[key] = data;
+        return this;
+    }
+
+    public output(): StepMeta & { substeps: StepMeta[] } {
+        return {
+            key: this.key,
+            time: this.time,
+            record: this.records,
+            result: this.result,
+            substeps: Object.values(this.subtrackers).map((subtracker) => subtracker.output())
+        }
+    }
+
+    public outputFlattened(): StepMeta[] {
+        const substeps = Object.values(this.subtrackers).map((subtracker) => subtracker.outputFlattened());
+        return [{
+            key: this.key,
+            time: this.time,
+            record: this.records,
+            result: this.result,
+        }].concat(substeps.flat());
+    }
+
+    public ganttUrl(): string {
+        const substeps = this.outputFlattened();
+
+        const maxEndTs = Math.max(...substeps.map((step) => step.time.endTs));
+  
+        const chartData = {
+            type: 'horizontalBar',
+            data: {
+                labels: substeps.map((step) => step.key),
+                datasets: [
+                    {
+                        data: substeps.map((step) => [
+                            (step.time.startTs - this.time.startTs),
+                            (step.time.endTs - this.time.startTs),
+                        ]),
+                    },
+                ],
+            },
+            options: {
+                legend: {
+                    display: false,
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            position: 'top',
+                            ticks: {
+                                min: 0,
+                                max: maxEndTs - this.time.startTs,
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+  
+        const chartUrl = `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(chartData))}`;
+        return chartUrl;
+    }
+}
