9dbd5861cf73cceaeda0e6bdcd210761eb422147
lokwkin <lokwkin@gmail.com>
2025-03-05 01:23:45 +0000

    feat: update storage adapter implementation

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,4 +1,4 @@
-import { Step } from './step';
+import { Step, StepMeta } from './step';
 import { v4 as uuidv4 } from 'uuid';
 import { StorageAdapter } from './storage/storage-adapter';
 
@@ -26,10 +26,24 @@ export class Pipeline extends Step {
 
     // Add event listener for completion to save data
     if (this.autoSave) {
-      this.on('step-complete', async (key, _) => {
+      this.on('step-start', async (key: string, stepMeta?: StepMeta) => {
         if (key === this.getKey()) {
-          // Only save when the root pipeline completes
-          await this.saveRun();
+          // This step marks the pipeline
+          await this.storageAdapter?.initiateRun(this);
+        }
+        if (!stepMeta) {
+          return;
+        }
+        await this.storageAdapter?.initiateStep(this.runId, stepMeta);
+      });
+      this.on('step-complete', async (key: string, stepMeta?: StepMeta) => {
+        if (!stepMeta) {
+          return;
+        }
+        await this.storageAdapter?.finishStep(this.runId, stepMeta);
+        if (key === this.getKey()) {
+          // This step marks the pipeline
+          await this.storageAdapter?.finishRun(this, stepMeta.error ? 'failed' : 'completed');
         }
       });
     }
@@ -41,11 +55,5 @@ export class Pipeline extends Step {
 
   public async track<T = any>(callable: (st: Step) => Promise<T>): Promise<T> {
     return await this.run(callable);
-  }
-
-  public async saveRun(): Promise<void> {
-    if (this.storageAdapter) {
-      await this.storageAdapter.saveRun(this);
-    }
   }
 }
