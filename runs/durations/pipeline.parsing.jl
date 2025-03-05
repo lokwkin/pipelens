4d4de109c194b278d39ba6f253d2a2dca4ac6e72
lokwkin <lokwkin@gmail.com>
2025-03-05 01:20:51 +0000

    feat: remove log files

--- runs/durations/pipeline.parsing.jl
+++
@@ -1,7 +0,0 @@
-{"timestamp":1740877461376,"value":4444,"stepKey":"pipeline.parsing","runId":"3729cd62-54ec-4d34-a0d1-cbb475d205d6"}
-{"timestamp":1740877466049,"value":2676,"stepKey":"pipeline.parsing","runId":"53769de3-723d-4586-a1a7-54c73ed663b5"}
-{"timestamp":1740877495083,"value":4788,"stepKey":"pipeline.parsing","runId":"2dd2ffc3-8c29-460d-83ad-b0735492acaa"}
-{"timestamp":1740877570097,"value":3885,"stepKey":"pipeline.parsing","runId":"934f824a-55d4-49e7-8b0e-8c6eab096c36"}
-{"timestamp":1740877657727,"value":4599,"stepKey":"pipeline.parsing","runId":"2f5f2a46-f7d3-4d9a-8480-64b315cb1ee7"}
-{"timestamp":1740877748670,"value":3772,"stepKey":"pipeline.parsing","runId":"9ac2a03e-e820-4a04-b121-b0cdf4822345"}
-{"timestamp":1740878073937,"value":4876,"stepKey":"pipeline.parsing","runId":"4f081307-58bd-4940-95e6-1d1b284dc1c6"}
--- runs/durations/pipeline.parsing.page_2.jl
+++
@@ -1,7 +0,0 @@
-{"timestamp":1740877461376,"value":2938,"stepKey":"pipeline.parsing.page_2","runId":"3729cd62-54ec-4d34-a0d1-cbb475d205d6"}
-{"timestamp":1740877466049,"value":1171,"stepKey":"pipeline.parsing.page_2","runId":"53769de3-723d-4586-a1a7-54c73ed663b5"}
-{"timestamp":1740877495083,"value":1588,"stepKey":"pipeline.parsing.page_2","runId":"2dd2ffc3-8c29-460d-83ad-b0735492acaa"}
-{"timestamp":1740877570097,"value":2226,"stepKey":"pipeline.parsing.page_2","runId":"934f824a-55d4-49e7-8b0e-8c6eab096c36"}
-{"timestamp":1740877657727,"value":3095,"stepKey":"pipeline.parsing.page_2","runId":"2f5f2a46-f7d3-4d9a-8480-64b315cb1ee7"}
-{"timestamp":1740877750173,"value":1235,"stepKey":"pipeline.parsing.page_2","runId":"9ac2a03e-e820-4a04-b121-b0cdf4822345"}
-{"timestamp":1740878075442,"value":2417,"stepKey":"pipeline.parsing.page_2","runId":"4f081307-58bd-4940-95e6-1d1b284dc1c6"}
